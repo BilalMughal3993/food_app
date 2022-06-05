6f7459ed7decdd4ac327c335ab9fc76e93ce0e3c
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-06-06 00:35:50 +0500

    creating a sreach bar is done

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { createAppContainer } from "react-navigation";
-import {createStackNavigator} from "react-navigation-stack"
+import { createStackNavigator } from 'react-navigation-stack';
+
+import SearchScreen from "./src/screens/SearchScreen";
 
 const navigator = createStackNavigator({
-  
-})
+  Search : SearchScreen
+},{
+    initialRouteName:"Search",
+    defaultNavigationOptions:{
+      title:"Business App"
+    }
+
+});
+
+export default createAppContainer(navigator);
---
+++ src/components/SearchBar.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View ,TextInput} from 'react-native'
+import React from 'react'
+import {Feather} from "@expo/vector-icons"
+
+const SearchBar = ({term,onTermChange,onTermSubmit}) => {
+  return (
+    <View style={styles.backGroundC}>
+        <Feather style={styles.iconStyle} name='search'/>
+        <TextInput 
+        placeholder='Search' 
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+        autoCapitalize="none"
+        autoCorrect="false"
+        />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({
+    backGroundC:{
+        backgroundColor:"#F0E0EE",
+        marginHorizontal:20,
+        borderRadius:10,
+        height:50,
+        marginTop:15,
+        flexDirection:"row"
+    },
+    inputStyle:{
+        flex:1,
+        fontSize:18,
+
+    },
+    iconStyle:{
+        fontSize:35,
+        alignSelf:"center",
+        marginHorizontal:15
+    }
+})
